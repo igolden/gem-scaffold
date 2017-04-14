d7b4ae0cadc0a7f4c8668aef43b9b8f9d196665b
Ian Golden <ian@iangolden.com>
2017-04-13 18:59:57 -0600

    Added more scaffolding

--- lib/scaffold.rb
+++ lib/scaffold.rb
@@ -1,5 +1,41 @@
-require "scaffold/version"
+#!/usr/bin/env ruby
 
-module Scaffold
-  # Your code goes here...
+require 'rubygems'
+require 'commander'
+
+class Scaffole
+  include Commander::Methods
+  # include whatever modules you need
+
+  def run
+    program :name, 'scaffold'
+i    program :version, '0.0.1'
+    program :description, ''
+
+    command :download_whitepapers do |c|
+      c.syntax = 'scaffold download_whitepapers [options]'
+      c.summary = ''
+      c.description = ''
+      c.example 'description', 'command example'
+      c.option '--some-switch', 'Some switch that does something'
+      c.action do |args, options|
+        p "DOWNLOADING"
+      end
+    end
+
+    command :flashcard do |c|
+      c.syntax = 'scaffold flashcard [options]'
+      c.summary = ''
+      c.description = ''
+      c.example 'description', 'command example'
+      c.option '--some-switch', 'Some switch that does something'
+      c.action do |args, options|
+        p "HELLO MAMA"
+      end
+    end
+
+    run!
+  end
 end
+
+Scaffold.new.run if $0 == __FILE__
